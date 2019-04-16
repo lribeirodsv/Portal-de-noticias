a9ee5796213ddd1c003d61b931ab73f89b3fb6eb
Lucas de Paula Ribeiro <lribeiro@scopus.com.br>
2019-04-16 08:01:12 -0300

    Routes refactoring
    Refactoring routes using consign module autoload

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 var app = require("./config/server"); 
 
-var rotaNoticias      = require('./app/routes/noticias')(app);
-var rotaHome          = require('./app/routes/home')(app);
-var rotaFormINoticias = require('./app/routes/formINoticia')(app);
-
-//var msg = require("./modulos/mod_teste"); 
-
 //sobe o servidor na porta 3000 e executa a funcao de callback
 console.log("[DEBUG] - Starting server...");
 app.listen(3000, function(){
--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
-//retorna uma funcao para a variavel express
-var express = require('express');                   
+var express = require('express');      
+var consign = require('consign');
 
-//executa a funcao retornada pelo express
 var app = express();                                
-
 app.set('view engine' , 'ejs');               //define o engine de views do express para que seja a engina do EJS
 app.set('views'       , './app/views');       //define o diretorio padrao para encontrar as views        
+
+consign().include('./app/routes').into(app);        //inclui todos os modulos de rotas no nosso servidor
 
 module.exports = app;
